2dcf6e36dd33ccf03ca9f8c747e5a4f4dec5310a
tomas <cctomass@gmail.com>
2019-12-10 00:06:00 +0200

    vuejs project

---
+++ socnet-web/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <h1>Welcome to SocNet</h1>
+    <el-row>
+      <el-col>
+        <router-link to="login"><el-button type="success" round>Login</el-button></router-link>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <router-link to="register"><el-button type="primary" round>Register new account</el-button></router-link>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <router-link to="home"><el-button type="warning" round>Browse as a guest</el-button></router-link>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import {Button, Col, Row} from 'element-ui'
+
+export default {
+  name: 'home',
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped>
+ .el-row {
+   margin-bottom: 10px;
+ }
+</style>
---
+++ socnet-web/src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id='app'>
+    <h1>My Todo App!</h1>
+    <el-input placeholder='Please input' v-model='input'></el-input>
+  </div>
+</template>
+
+<script>
+import { Input } from 'element-ui';
+
+export default {
+  components: {
+    Input
+  }
+}
+</script>
